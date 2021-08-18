3a4225acf5354cfdf02d160649cba39759d86f94
A01641937-alf <A01641937@itesm.mx>
2021-08-18 21:56:45 +0000

    actividades

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    numero1 = int(input("Dame un número: "))
+    numero2 = int(input("Dame un número: "))
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    mult = numero1 * numero2
+    print("Suma:",suma)
+    print("Resta:",resta)
+    print("Multiplicación:",mult)
+
+
 
 
 
